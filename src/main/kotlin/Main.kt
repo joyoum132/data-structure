e45ef9a9723349c895d7223ae8fb3600e12d2258
jyyoo <jyyoo@autocrypt.io>
2023-09-12 02:28:10 +0900

    dfs, bfs 구현

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -31,4 +31,30 @@ fun callCh14() {
         addEdge(2, 4)
         printEdge()
     }
+
+    println("""
+
+        연결리스트로 구현한 DFS 그래프
+    """.trimIndent())
+    val graph = LinkedLIstSimpleGraph(7).apply {
+        addEdge(0,1)
+        addEdge(0,3)
+        addEdge(1,0)
+        addEdge(1,2)
+        addEdge(2,1)
+        addEdge(2,3)
+        addEdge(3,0)
+        addEdge(3,2)
+        addEdge(3,4)
+        addEdge(4,3)
+        addEdge(4,5)
+        addEdge(4,6)
+        addEdge(5,4)
+        addEdge(6,4)
+        printEdge()
+    }
+
+    graph.dfs(0)
+    graph.recursiveDfs(0)
+    graph.bfs(4)
 }
--- src/main/kotlin/ch14_graph/LinkedLIstSimpleGraph.kt
+++ src/main/kotlin/ch14_graph/LinkedLIstSimpleGraph.kt
@@ -1,13 +1,75 @@
 package ch14_graph
-import java.util.LinkedList
+import java.util.*
 
 class LinkedLIstSimpleGraph(node: Int) {
     private val adjacencyList: Array<LinkedList<Int>> = Array(node) { LinkedList() }
+
+    private val visited = BooleanArray(node) { false } //node 방문 여부
+    private var stack = Stack<Int>()
+    private var queue: Queue<Int> = LinkedList()
 
     fun addEdge(start: Int, end: Int) {
         adjacencyList[start].add(end)
     }
 
+    fun dfs(start: Int) {
+        stack.push(start)
+        while (stack.isNotEmpty()) {
+            /*
+            * 방문하지 않은 노드라면 방문 처리를 완료하고
+            * 해당 노드와 인접한 모든 노드를 스택에 넣는다.
+            * 스택은 FILO 이기때문에 탐색의 방향이 leaf로 간다
+            * */
+            val current = stack.pop()
+            if(!visited[current]) {
+                visited[current] = true
+                println("${(current+65).toChar()} 방문")
+                val itr = adjacencyList[current].iterator()
+                while(itr.hasNext()) {
+                    val n = itr.next()
+                    if(!visited[n]) stack.push(n)
+                }
+            }
+        }
+    }
+
+    fun recursiveDfs(start: Int) {
+        visited[start] = true
+        println("${(start+65).toChar()} 방문")
+
+        val iterator = adjacencyList[start].iterator()
+        while(iterator.hasNext()) {
+            val next = iterator.next()
+            if(!visited[next]) {
+                recursiveDfs(next)
+            }
+        }
+    }
+
+    fun bfs(start: Int) {
+        queue.add(start)
+
+        while(queue.isNotEmpty()) {
+            val n = queue.poll()
+//                queue.remove() // queue == isEmpty 인 경우 throw
+
+            if(!visited[n]) {
+                visited[n] = true
+                println("${(n+65).toChar()} 방문")
+
+                val iterator = adjacencyList[n].iterator()
+                while(iterator.hasNext()) {
+                    val next = iterator.next()
+                    /* FIFO 인 큐를 사용하기때문에 형제노드를 먼저 탐색하고
+                    * 그 이후에 자녀 노드를 탐색하여 leaf까지 간다.
+                    * */
+                    if(!visited[next]) {
+                        queue.add(next)
+                    }
+                }
+            }
+        }
+    }
     fun printEdge() {
         adjacencyList.forEachIndexed { index, ints ->
             ints.forEach {
